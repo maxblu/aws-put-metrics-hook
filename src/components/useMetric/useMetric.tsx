ca0d2c25c32976239df97d05b2707c3bb00c7126
Daniel <delaosa@datamart.cl>
2022-07-23 00:17:31 -0400

    init

---
+++ src/components/useMetric/useMetric.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  CloudWatchClient,
+  PutMetricDataCommand,
+  
+} from "@aws-sdk/client-cloudwatch";
+
+interface UseMetricConfig {
+    namesapce: string,
+    region: string,
+    credentials: Credentials
+}
+
+interface Credentials {
+    accessKeyId: string,
+    secretAccessKey: string,
+    sessionToken: string | undefined
+}
+
+interface DataPoints {
+    metricName: string,
+    value: number
+}
+
+
+
+function useMetric(config:UseMetricConfig) {
+    const [cwClient, _ ] = useState(new CloudWatchClient(config));
+//   const [results, setResults] = useState([]);
+//   const [error, setError] = useState("");
+
+    const  sendMetric = async (points:[DataPoints]) =>{
+        
+        const command = new PutMetricDataCommand({
+        Namespace: config.namesapce,
+        MetricData: points.map( item => {
+            return {
+                MetricName: item.metricName,
+                StorageResolution: 1,
+                Unit: "Seconds",
+                Value: item.value,
+            }
+        })
+    });
+        return await cwClient.send(command)
+
+    }
+
+  return {
+    sendMetric
+  };
+}
+
+export default useMetric;
